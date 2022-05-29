1180b5d91ff1343db8fd0465fcba6f974e54e171
kovt <kovtunovsky@gmail.com>
2022-05-29 20:54:34 +0300

    create HW_1

---
+++ HW_1/script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+cd folder03
+mkdir folder001 folder002 folder003
+cd folder001
+touch file_1.txt file_2.txt file_3.txt file_1.json file_2.json
+mkdir folder0001 folder0002 folder0003
+ls -la
+mv folder001/file_1.txt folder001/file_2.txt folder002
